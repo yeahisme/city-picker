1c4a83c637393d15697b0e0c909da2597ca9df14
q.chen <q.chen@Ctrip.com>
2016-10-28 19:24:25 +0800

    init city search panel

---
+++ src/CitySearchPanel.js
@@ -0,0 +1,18 @@
+const classnames = require('classnames');
+import React, { Component } from 'react';
+import { List } from 'immutable';
+import Animate from 'rc-animate';
+
+class CitySearchPanel extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default CitySearchPanel;
